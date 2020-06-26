0464183d6f49c54af6a4be7cae1ad92112a5ed36
SmithSE <79119779556@ya.ru>
2020-06-26 15:48:18 +0300

    Download my example of the profram ParseTableFiles

--- PyramidBuilder/src/main/java/Main.java
+++
@@ -1,8 +0,0 @@
-import java.util.Arrays;
-
-public class Main {
-    public static void main(String[] args) {
-        PyramidBuilder pyramidBuilder = new PyramidBuilder();
-        pyramidBuilder.printPyramid(pyramidBuilder.buildPyramid(Arrays.asList(1, 5, 9, 2, 3, 4)));
-    }
-}
--- PyramidBuilder/src/main/java/exception/CannotBuildException.java
+++
@@ -1,4 +0,0 @@
-package exception;
-
-public class CannotBuildException extends RuntimeException {
-}
